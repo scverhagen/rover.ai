b5b330564fcdbdc4d908108ccc82a156b02465ea
Steve Verhagen <scverhagen@gmail.com>
2019-04-15 23:10:49 -0500

    start roverwww.py

---
+++ roverwww.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import os
+import rovercom
+from flask import Flask, request, send_file
+app = Flask(__name__)
+
+thisfilepath = os.path.dirname(__file__)
+
+@app.route('/')
+def wwwroot():
+    hs = ''
+    hs += '<html><body>'
+    hs += '</body></html>'
+    return hs
+
+@app.route('/sendcommand')
+def wwwsendcommand():
+    cmd = request.args.get('cmd')
+    rovercom.sendcommand(cmd)
+    return cmd
+
+#needs to be at end of file:
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8088, threaded=True)
